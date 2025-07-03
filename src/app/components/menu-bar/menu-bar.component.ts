954e519ed0b6b983a9063053de5319de09bd1dc1
Carlos Daniel <144691269+KarlosNiel@users.noreply.github.com>
2025-07-02 21:43:20 -0300

    Atribuindo  uma responsividade basica aos componentes, e mudando o tipo de componente de menu-bar para um menubar de fato em vez de toolbar.

--- src/app/components/menu-bar/menu-bar.component.ts
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
-import { ToolbarModule } from 'primeng/toolbar';
-import { ButtonModule } from 'primeng/button';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 import { AvatarModule } from 'primeng/avatar';
+import { InputTextModule } from 'primeng/inputtext';
+
+import { Menubar } from 'primeng/menubar';
+
 
 @Component({
   selector: 'app-menu-bar',
-  imports: [ToolbarModule, ButtonModule, AvatarModule,],
+  imports: [Menubar, AvatarModule, InputTextModule],
   templateUrl: './menu-bar.component.html',
   styleUrl: './menu-bar.component.css'
 })
-export class MenuBarComponent {
-  menuAberto: boolean = false;
+export class MenuBarComponent implements OnInit {
+    items: MenuItem[] | undefined;
+
+    ngOnInit() {
+      this.items = [
+        {
+          label: 'HOME',
+        },
+        {
+          label: 'BLOG',
+        },
+        {
+          label: 'BOOKMARKS'
+        },
+        {
+          label: 'UI KIT'
+        }
+      ]
+    }
 }
